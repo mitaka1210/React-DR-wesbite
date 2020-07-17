e31e5d2133c739161636f0f2f03107b127ed1349
Dimitar <dimitar_dimitrov12@mail.bg>
2020-07-17 09:34:57 +0300

    Updates

---
+++ precache-manifest.9724e36a39457a0526464a60dae7dfce.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3fd97eef5f5e7bd67e0052ed53bd2f15",
+    "url": "/React-DR-wesbite/index.html"
+  },
+  {
+    "revision": "144a76693b9dc29824fa",
+    "url": "/React-DR-wesbite/static/css/main.bb4c2081.chunk.css"
+  },
+  {
+    "revision": "57ac69c8bfaec465723d",
+    "url": "/React-DR-wesbite/static/js/2.6fb769e2.chunk.js"
+  },
+  {
+    "revision": "dd207babaeb913476be46155cc504ef5",
+    "url": "/React-DR-wesbite/static/js/2.6fb769e2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "144a76693b9dc29824fa",
+    "url": "/React-DR-wesbite/static/js/main.d6468a43.chunk.js"
+  },
+  {
+    "revision": "6d538fb934185a467769",
+    "url": "/React-DR-wesbite/static/js/runtime-main.3d4ab8f6.js"
+  },
+  {
+    "revision": "407bd1fae6079b6cf3de132b4c881657",
+    "url": "/React-DR-wesbite/static/media/about.407bd1fa.jpg"
+  },
+  {
+    "revision": "789bcbafaed564af8e5b063868f419d9",
+    "url": "/React-DR-wesbite/static/media/dental-1.789bcbaf.svg"
+  },
+  {
+    "revision": "8a7121abfbb71df6b66a49d49bb5c2c8",
+    "url": "/React-DR-wesbite/static/media/img-1.8a7121ab.jpg"
+  },
+  {
+    "revision": "0c40349b2670c5bc7cd73d54ee4970ed",
+    "url": "/React-DR-wesbite/static/media/img-2.0c40349b.jpg"
+  },
+  {
+    "revision": "d85001e857955af77def7ed1f19c0548",
+    "url": "/React-DR-wesbite/static/media/img-4.d85001e8.jpg"
+  },
+  {
+    "revision": "064ca7187df9235b933aa5d603de091f",
+    "url": "/React-DR-wesbite/static/media/img-5.064ca718.jpg"
+  }
+]);
